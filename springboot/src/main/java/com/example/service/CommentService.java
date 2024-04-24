79473482eac77e9ba74f6fb7891088637d99da59
xuanchen <1737739958@qq.com>
2024-04-24 15:15:08 +0800

    店铺评论信息管理后端

---
+++ springboot/src/main/java/com/example/service/CommentService.java
@@ -0,0 +1,89 @@
+package com.example.service;
+
+import com.example.common.enums.RoleEnum;
+import com.example.entity.Account;
+import com.example.entity.Comment;
+import com.example.mapper.CommentMapper;
+import com.example.utils.TokenUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 评价表业务处理
+ **/
+@Service
+public class CommentService {
+
+    @Resource
+    private CommentMapper commentMapper;
+
+    /**
+     * 新增
+     */
+    public void add(Comment comment) {
+        commentMapper.insert(comment);
+    }
+
+    /**
+     * 删除
+     */
+    public void deleteById(Integer id) {
+        commentMapper.deleteById(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            commentMapper.deleteById(id);
+        }
+    }
+
+    /**
+     * 修改
+     */
+    public void updateById(Comment comment) {
+        commentMapper.updateById(comment);
+    }
+
+    /**
+     * 根据ID查询
+     */
+    public Comment selectById(Integer id) {
+        return commentMapper.selectById(id);
+    }
+
+    /**
+     * 查询所有
+     */
+    public List<Comment> selectAll(Comment comment) {
+        // 拿到当前的登录用户信息
+        Account currentUser = TokenUtils.getCurrentUser();
+        String role = currentUser.getRole();
+        if (RoleEnum.BUSINESS.name().equals(role)) { // 当角色是商家时，只能查询到商品分类的businessName为该商家的商品
+            comment.setBusinessId(currentUser.getId());
+        }
+        return commentMapper.selectAll(comment);
+    }
+
+    /**
+     * 分页查询
+     */
+    public PageInfo<Comment> selectPage(Comment comment, Integer pageNum, Integer pageSize) {
+        // 拿到当前的登录用户信息
+        Account currentUser = TokenUtils.getCurrentUser();
+        String role = currentUser.getRole();
+        if (RoleEnum.BUSINESS.name().equals(role)) { // 当角色是商家时，只能查询到商品分类的businessName为该商家的商品
+            comment.setBusinessId(currentUser.getId());
+        }
+        PageHelper.startPage(pageNum, pageSize);
+        List<Comment> list = commentMapper.selectAll(comment);
+        return PageInfo.of(list);
+    }
+
+}
